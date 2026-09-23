8b22be734cfc93361ff750d5a2434cfd7d50400b
hope-corp <kelvinoduro544@gmail.com>
2026-09-23 02:36:58 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ group = "com.papaguycodes"
 version = "0.0.1"
 
 application {
-    // Note: Package name uses underscore instead of hyphen for JVM compatibility
     mainClass.set("com.papaguycodes.driver_license_generator.ApplicationKt")
 }
 
@@ -21,9 +20,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-status-pages-jvm")
     implementation("io.ktor:ktor-server-cors-jvm")
     implementation("ch.qos.logback:logback-classic:1.4.14")
-    
-    testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
